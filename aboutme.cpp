e771c84d273e6de12b41cb22782c08857b76ab6f
sudosurootdev <sudosurootdev81@gmail.com>
2018-07-11 18:57:49 -0400

    Added checks for correct input and std::cerr messages in aboutme.cpp
    Change-Id: I3fccd971bee984616b57b850543a8b33de9ca4b7
    Signed-off-by: sudosurootdev <sudosurootdev81@gmail.com>

--- aboutme.cpp
+++ aboutme.cpp
@@ -11,14 +11,26 @@ int main()
     int month;
     std::cin >> month;
     std::cout << std::endl;
+    if (month > 12 || month <= 0) {
+	std::cerr << "You must enter a correct month (1 - 12)!!!" << std::endl;
+	std::cout << std::endl;
+    }
     std::cout << "Please enter your birth day (ex. 1 - 31): ";
     int day;
     std::cin >> day;
     std::cout << std::endl;
+    if (day > 31 || day <= 0) {
+        std::cerr << "You must enter a correct day (1 - 31)!!!" << std::endl;
+        std::cout << std::endl;
+    }
     std::cout << "Please enter your birth year (ex. 1900 - 2018): ";
     int year;
     std::cin >> year;
     std::cout << std::endl;
+    if (year > 2018 || year <= 0) {
+        std::cerr << "You must enter a correct year (1900 - 2018)!!!" << std::endl;
+        std::cout << std::endl;
+    }
     std::cout << std::endl;
     std::cout << "My name is: " << std::endl;
     std::cout << name << std::endl;
