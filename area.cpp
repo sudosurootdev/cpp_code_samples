a8d6b1d1f7868ccf102898e87518487f6c44c8ff
sudosurootdev <sudosurootdev81@gmail.com>
2018-07-13 18:49:14 -0400

    Day 3: LISTING 3.2 A Demonstration of the Use of Variables
    Change-Id: I144946f90ac2607513511ab6ef41e583ad86817f
    Signed-off-by: sudosurootdev <sudosurootdev81@gmail.com>

---
+++ area.cpp
@@ -0,0 +1,16 @@
+// Demonstration of variables
+#include <iostream>
+
+int main() {
+
+    unsigned short int Width = 5, Length = 10;
+
+    // create an unsigned short int and initialize with result
+    // of multiplying Width by Length
+    unsigned short int Area = (Width * Length);
+
+    std::cout << "Width: " << Width << std::endl;
+    std::cout << "Length: " << Length << std::endl;
+    std::cout << "Area: " << Area << std::endl;
+    return 0;
+}
