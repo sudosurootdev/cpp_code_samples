fbcd7bf456d47abec3bddf9c6254107e4c8c35ed
sudosurootdev <sudosurootdev81@gmail.com>
2018-07-15 14:13:20 -0400

    Day 4: LISTING 4.1 Evaluating Complex Expressions
    Change-Id: I3a5467bf2fdefc297fe4334a28dd736c16077320
    Signed-off-by: sudosurootdev <sudosurootdev81@gmail.com>

---
+++ complex-expressions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main() {
+    using std::cout;
+    using std::endl;
+    int a=0, b=0, x=0, y=35;
+    cout << "a: " << a << " b: " << b;
+    cout << " x: " << x << " y: " << y << endl;
+    a = 9;
+    b = 7;
+    y = x = a+b;
+    cout << "a: " << a << " b: " << b;
+    cout << " x: " << x << " y: " << y << endl;
+    return 0;
+}
